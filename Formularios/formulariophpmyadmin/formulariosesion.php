4f353735df8b32b82ccc3183185ae579469159cd
ERMASMASCAO <danielcamiansadm@gmail.com>
2022-10-03 13:53:19 +0200

    cambios

--- Formularios/formulariophpmyadmin/formulariosesion.php
+++ Formularios/formulariophpmyadmin/formulariosesion.php
@@ -1,3 +1,26 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $opciones = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_PERSISTENT => true
+    );
+
+    $pdo = new PDO(
+        'mysql:host=localhost;dbname=proyecto1;charset=utf8',
+        'root',
+        'sa',
+    $opciones);
+
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $registros = $pdo->exec("INSERT INTO users(username,email,password) VALUES('$username','$email','$password')");
+
+    echo "<p>Se ha registrado correctamene.</p>";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,10 +30,22 @@
     <title>formulariosesionpHPMYADMIN</title>
 </head>
 <body>
-    Tu nombre de usuari@ <?php echo $_POST["nombre"]; ?><br>
-    Correo Electrónico <?php echo $_POST["correo"]; ?><br>
-    Contraseña 
-    <?php echo $_POST["contraseña"]; ?><br>
-    <?php echo["confirmarContraseña"];?><br>
+<form action="" method="post" enctype="multipart/form-data">
+    Nombre de usuari@ 
+        <input type="text" name="username"><br>
+
+    Correo electónico 
+        <input type="correo" name="email"><br>
+
+    Contraseña
+        <input type="contraseña" name="password"><br>
+
+    Confirmar contraseña
+        <input type="confirmarContraseña" name="confirmarContraseña"><br>
+    
+        <input type="submit" value="Registrarse" name="enviar"><br>
+
+    ¿Ya eres miembro? Acceso Usuari@s<br>
+
 </body>
 </html>
