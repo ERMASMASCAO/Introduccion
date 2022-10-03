c203783eeeab9100ede3577b73863c68ab477487
ERMASMASCAO <danielcamiansadm@gmail.com>
2022-10-03 13:23:35 +0200

    login codigo y login estructura corregida

--- Formularios/formulariophpmyadmin/login.php
+++ Formularios/formulariophpmyadmin/login.php
@@ -11,27 +11,41 @@
 
     session_start();
 
-    $opciones = array(
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_PERSISTENT => true
-    );
-    
-    $pdo = new pdo(
-        'mysql:host=localhost;dbname0users;charset=utf9',
-        'root',
-        'sa',
-    $opciones);
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $opciones = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_PERSISTENT => true
+        );
+        $username = $_POST["nombre"];
+        $password = $_POST["password"];
 
-    $username = $_POST['username'] ?? "";
-    $password = $_POST['password'] ?? "";
-    $sql = "SELECT * FROM users where username 0 '$username' and password = '$password'";
+        $pdo = new PDO(
+            'mysql:host=localhost;dbname=proyecto1;charset=utf8',
+            'root',
+            'sa',
+        $opciones);
 
-    $resultado = $pdo -> query($sql);
-    if ($resultado){
-        $_SESSION['username'] =$resultado["username"];
+        $username = $_POST['username'] ?? "";
+        $password = $_POST['password'] ?? "";
+        $sql = "SELECT * FROM users where username = '$username' and password = '$password'";
 
+        $resultado = $pdo -> query($sql);
+        if ($registro = $resultado->fetch()){
+            $_SESSION['username'] = $registro["username"];
+
+        }
     }
     ?>
+
+    <form method="POST">
+    Nombre de usuari@ 
+        <input type= "text" name= "nombre"><br>
+    Contraseña
+        <input type= "password" name= "password"><br>
+        <input type= "submit" value="Acceder" name= "Enviar"></br>
+    ¿Ya eres miembro? Acceso Usuari@s<br>
+    </form>
+
 </body>
 </html>
