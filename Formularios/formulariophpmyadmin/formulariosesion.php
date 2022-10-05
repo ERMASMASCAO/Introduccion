83156f3a24347e4eceec4b5de3108b1192c2ec90
ERMASMASCAO <danielcamiansadm@gmail.com>
2022-10-05 09:04:10 +0200

    FORMULARIOPHPMYADMIN  TERMINADO

--- Formularios/formulariophpmyadmin/formulariosesion.php
+++ Formularios/formulariophpmyadmin/formulariosesion.php
@@ -26,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8">
     <title>formulariosesionpHPMYADMIN</title>
 </head>
 <body>
