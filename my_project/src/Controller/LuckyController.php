a81e32094a4f58c128e69532f878111c1f0009d9
ERMASMASCAO <danielcamiansadm@gmail.com>
2022-10-05 09:46:44 +0200

    Primeros pasos Symfony hecho.

---
+++ my_project/src/Controller/LuckyController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class LuckyController
+{
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>');
+    }
+}
+?>
