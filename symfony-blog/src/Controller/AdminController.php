c825897eb95bb47e295cf94bfe753c54c7fc0e62
ERMASMASCAO <danielcamiansadm@gmail.com>
2022-11-11 12:39:49 +0100

    4.4.4 empezar

--- symfony-blog/src/Controller/AdminController.php
+++ symfony-blog/src/Controller/AdminController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\CategoryFormType;
 
 class AdminController extends AbstractController
 {
@@ -15,4 +19,32 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/images.html.twig', []);
     }
+
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repositorio = $doctrine->getRepository(Category::class);
+    
+        $categories = $repositorio->findAll();
+    
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories   
+        ));
+    
+    }
+    
+    
+
+
+
 }
